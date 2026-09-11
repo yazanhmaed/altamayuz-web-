0adfe439d7150589bde58edb70a5b74bee2c63bd
yazanhmaed <yhmaed58@gmail.com>
2026-09-11 22:31:59 +0300

    fix

--- storefront/lib/main.dart
+++ storefront/lib/main.dart
@@ -2,12 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'firebase_options.dart';
 import 'router.dart';
 import 'theme/app_theme.dart';
 
 Future<void> main() async {
+  usePathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const StorefrontApp());
